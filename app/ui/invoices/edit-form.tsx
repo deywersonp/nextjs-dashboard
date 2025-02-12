7b2cd15da5d67bc4f1c603a4a077756274f40c2b
Deywerson Pereira <deywerson.pereira@gmail.com>
2025-02-12 18:06:01 -0300

    feat: added edit invoice page and action

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,8 +18,15 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  //The first parameter is null because when using bind the expected first value
+  //is a "this" context (e.g. object or class).
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
+  //the code above is the same of
+  // const updateInvoiceWithId = (formData: FormData) => updateInvoice(invoice.id, formData);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
